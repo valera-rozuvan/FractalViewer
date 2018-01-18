a0b5d17d6d9471d755634b717bd2a3ab81834184
Valera Rozuvan <valera.rozuvan@gmail.com>
2018-01-18 18:35:43 +0200

    Fix export to png error

--- js/FractalRunner.js
+++ js/FractalRunner.js
@@ -91,7 +91,23 @@ define('FractalRunner', ['bootstrap-dialog', 'jquery'], function(BootstrapDialog
     $('#controlToggler').on('click', toggleControlsForm);
     $('#fractalTypesBtn').on('click', toggleFractalTypesDialog);
     $('#exportToPngBtn').on('click', function() {
-      window.open(exportToPNG(), '_newtab');
+      // window.open(exportToPNG(), '_newtab');
+
+      // We can't simply open a new blank window with the image.
+      // Apparently Google Chrome has removed support for top-frame
+      // navigation, you can see more informations here:
+      // https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/GbVcuwg_QjM
+
+      // So, to get around this problem, we open a new window, and inside it create
+      // an IFrame with our image.
+
+      var win = window.open();
+      win.document.write(
+        '<iframe src="' + exportToPNG() + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
+      );
+
+      
+
       return false;
     });
     $('#aboutDialogButton').on('click', toggleAboutDialog);
